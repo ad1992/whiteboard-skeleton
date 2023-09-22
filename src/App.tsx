75c80b1b33c916c54dc2f62f3adecc4715e12fbf
Aakansha Doshi <aakansha1216@gmail.com>
2023-09-23 01:37:12 +0530

    update skeleton with canvas ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.scss";
 import ShapesToolBar, { ActiveTool } from "./ShapesToolBar";
 import { TrashIcon } from "./icons";
 
 function App() {
   const [activeTool, setActiveTool] = useState<ActiveTool>("selection");
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   return (
     <div className="whiteboard">
       <div className="canvas-container">
-        <canvas id="canvas" />
+        <canvas ref={canvasRef} id="canvas" />
       </div>
       <ShapesToolBar
         onClick={(tool: ActiveTool) => {
